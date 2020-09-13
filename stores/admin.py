4df694f1d2444c27738676be29845a947640712b
ghadeer <apps02977@gmail.com>
2020-09-13 18:33:36 +0300

    Done task django_advanced_02

--- stores/admin.py
+++ stores/admin.py
@@ -4,4 +4,5 @@ from .models import Store
 # Register your models here.
 class StoreAdmin(admin.ModelAdmin):
     list_display=('id' ,'name' ,'slug' ,'description',)
+    list_editable = ('description',)
 admin.site.register(Store, StoreAdmin)
